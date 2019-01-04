7090e59edf08e8ec43f32a9dde17b029fe47bf4e
Abhishek Mishra <abhishekmishra3@gmail.com>
2019-01-04 23:15:55 +0530

    added sasm dockerfile and run script

---
+++ sasm/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+docker run -it --privileged \
+    --net host \
+    --memory 512mb \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    --volume="$HOME/.Xauthority:/root/.Xauthority:rw" \
+    --env="DISPLAY" \
+    -v $HOME/programs/asm_programs:/root/asm_programs \
+    -v "$HOME/.config/SASM Project/SASM.conf":"/root/.config/SASM Project/SASM.conf" \
+    -e QT_X11_NO_MITSHM=1 \
+    --name sasm \
+    abhishekmishra/sasm:latest sasm
