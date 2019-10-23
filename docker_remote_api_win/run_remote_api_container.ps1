6257541869616d4c0a881b61654b8b079c74baa9
Abhishek Mishra <abhishekmishra3@gmail.com>
2019-10-23 13:28:26 +0530

    added comment containing firewall rule

--- docker_remote_api_win/run_remote_api_container.ps1
+++ docker_remote_api_win/run_remote_api_container.ps1
@@ -1,1 +1,4 @@
+# firewall rule is
+# netsh advfirewall firewall add rule name="docker_daemon" dir=in action=allow protocol=TCP localport=2376
+ 
 docker run -d -p 2376:2375 -v /var/run/docker.sock:/var/run/docker.sock jarkt/docker-remote-api
